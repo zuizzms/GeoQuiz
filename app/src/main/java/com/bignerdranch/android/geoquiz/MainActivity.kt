b0744bf2b5730a18ddd2709ab393255dbc14f593
Zuizz Saeed <zuizzms@bu.edu>
2024-02-04 14:51:45 -0500

    added snackbar

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.bignerdranch.android.geoquiz
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.snackbar.BaseTransientBottomBar
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,18 +22,18 @@ class MainActivity : AppCompatActivity() {
         falseButton = findViewById(R.id.false_button)
 
         trueButton.setOnClickListener { view: View ->
-            Toast.makeText(
-                this,
+            Snackbar.make(
+                view,
                 R.string.correct_toast,
-                Toast.LENGTH_SHORT)
+                BaseTransientBottomBar.LENGTH_SHORT)
                 .show()
         }
 
         falseButton.setOnClickListener { view: View ->
-            Toast.makeText(
-                this,
+            Snackbar.make(
+                view,
                 R.string.incorrect_toast,
-                Toast.LENGTH_SHORT)
+                BaseTransientBottomBar.LENGTH_SHORT)
                 .show()
         }
     }
